2aa586c1596e78fa77f9478fe1383e1eb97cbc17
GiorgioFantilli <giorgio.fantilli@studio.unibo.it>
2026-01-04 00:20:30 +0100

    feat: impl. proper builder for nn

---
+++ src/main/scala/domain/network/NetworkBuilder.scala
@@ -0,0 +1,46 @@
+package domain.network
+
+import domain.data.LinearAlgebra.*
+import scala.util.Random
+
+case class LayerConf(neurons: Int, activation: Activation)
+
+object NetworkBuilder:
+
+  def fromInputs(features: Feature*): Builder =
+    Builder(features.toList, List.empty)
+
+  case class Builder(
+    features: List[Feature],
+    hiddenLayers: List[LayerConf]
+  ):
+    def addLayer(neurons: Int, activation: Activation): Builder =
+      val newConf = LayerConf(neurons, activation)
+      this.copy(hiddenLayers = this.hiddenLayers :+ newConf)
+
+    def build(): Model =
+      require(features.nonEmpty, "No input features defined")
+
+      val rand = new Random()
+      val inputSize = features.length
+
+      def initWeights(rows: Int, cols: Int, standardDeviation: Double): Matrix =
+        Matrix.fill(rows, cols) {
+          rand.nextGaussian() * standardDeviation
+        }
+
+      val (layers, lastSize) = hiddenLayers.foldLeft((List.empty[Layer], inputSize)) {
+        case ((accLayers, inSize), conf) =>
+          val stdDev = conf.activation.standardDeviation(inSize, conf.neurons)
+          val w = initWeights(conf.neurons, inSize, stdDev)
+          val b = Vector.zeros(conf.neurons)
+          (accLayers :+ Layer(w, b, conf.activation), conf.neurons)
+      }
+
+      val outAct = summon[Activation](using Activations.sigmoid)
+      val outW = initWeights(1, lastSize, outAct.standardDeviation(lastSize, 1))
+      val outB = Vector.zeros(1)
+
+      val finalNetwork = Network(layers :+ Layer(outW, outB, outAct))
+
+      Model(finalNetwork, features)
---
+++ src/test/scala/domain/network/NetworkBuilderTest.scala
@@ -0,0 +1,53 @@
+package domain.network
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+
+import domain.data.Point2D
+import domain.network.Activations.given
+
+class NetworkBuilderTest extends AnyFunSuite with Matchers {
+
+  test("Builder correctly stores input features") {
+    NetworkBuilder.fromInputs(Feature.X, Feature.Y).features shouldBe List(Feature.X, Feature.Y)
+  }
+
+  test("Builder accumulates layer configurations with correct activations") {
+    NetworkBuilder.fromInputs(Feature.X)
+      .addLayer(10, Activations.relu)
+      .hiddenLayers.head.activation.name shouldBe "ReLU"
+  }
+
+  test("Build generates a Model with input dimension matching feature count") {
+    val model = NetworkBuilder.fromInputs(Feature.X, Feature.Y, Feature.ProductXY)
+      .addLayer(5, Activations.tanh)
+      .build()
+
+    model.network.layers.head.weights.cols shouldBe 3
+  }
+
+  test("Build forces Sigmoid activation on the output layer") {
+    val model = NetworkBuilder.fromInputs(Feature.X)
+      .addLayer(5, Activations.relu)
+      .build()
+
+    model.network.layers.last.activation.name shouldBe "Sigmoid"
+  }
+
+  test("Model incorporates Feature transformation in prediction pipeline") {
+    val model = NetworkBuilder.fromInputs(Feature.X, Feature.Y)
+      .addLayer(2, Activations.sigmoid)
+      .build()
+
+    model.predict(Point2D(1.0, 1.0)) should (be >= 0.0 and be <= 1.0)
+  }
+
+  test("Built network propagates forward pass without runtime errors") {
+    val model = NetworkBuilder.fromInputs(Feature.X)
+      .addLayer(10, Activations.relu)
+      .addLayer(10, Activations.tanh)
+      .build()
+
+    noException should be thrownBy model.predict(Point2D(0.5, 0.5))
+  }
+}
