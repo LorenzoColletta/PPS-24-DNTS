7e066b541ee341f1aefdd4de91b161ee98e64762
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-01-20 15:59:53 +0100

    feat: defines cluster commands

---
+++ src/main/scala/actors/cluster/ClusterCommand.scala
@@ -0,0 +1,34 @@
+package actors.cluster
+
+import akka.cluster.ClusterEvent.{MemberEvent, ReachabilityEvent}
+
+/**
+ * Defines the internal protocol of messages handled by cluster behaviors.
+ */
+sealed trait ClusterCommand
+
+/**
+ * Wraps an Akka Cluster [[MemberEvent]] for internal handling.
+ *
+ * @param e
+ * The original cluster member event (e.g., [[akka.cluster.ClusterEvent.MemberUp]], [[akka.cluster.ClusterEvent.MemberRemoved]])
+ */
+final case class MemberCommand(e: MemberEvent) extends ClusterCommand
+
+/**
+ * Wraps an Akka Cluster [[ReachabilityEvent]] for internal handling.
+ *
+ * @param e
+ * The original cluster reachability event (e.g., [[akka.cluster.ClusterEvent.ReachableMember]], [[akka.cluster.ClusterEvent.UnreachableMember]])
+ */
+final case class ReachabilityCommand(e: ReachabilityEvent) extends ClusterCommand
+
+/**
+ * Signals that the cluster simulation should start.
+ */
+case object StartSimulation extends ClusterCommand
+
+/**
+ * Signals that the cluster simulation should stop.
+ */
+case object StopSimulation extends ClusterCommand
