61374be54e9539cab9e10870dd5fbc35f0f010ab
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-01-20 21:13:24 +0100

    feat: implementation ModelActorTest

--- src/test/scala/actors/ModelActorTest.scala
+++ src/test/scala/actors/ModelActorTest.scala
@@ -1,11 +1,16 @@
 package actors
 
 import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
-import domain.data.Point2D
-import domain.network.{Activations, Feature, Model, ModelBuilder}
+import domain.network.{Activations, Feature, Model, ModelBuilder, Regularization}
+import domain.training.Strategies
 import org.scalatest.wordspec.AnyWordSpecLike
+import actors.ModelActor.ModelCommand
+import domain.training.Consensus.averageWith
 
 class ModelActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+
+  private val noReg = Strategies.Regularizers.fromConfig(Regularization.None)
+  private val testOptimizer = new Strategies.Optimizers.SGD(learningRate = 0.01, reg = noReg)
 
   def createModel(seed: Long): Model =
     ModelBuilder.fromInputs(Feature.X, Feature.Y)
@@ -15,40 +20,56 @@ class ModelActorTest extends ScalaTestWithActorTestKit with AnyWordSpecLike {
 
   "A ModelActor" should {
 
-    "return different predictions after a model update" in {
+    "return the current model when requested" in {
+      val model1 = createModel(1L)
+      val modelActor = spawn(ModelActor(model1, testOptimizer))
+      val probe = createTestProbe[Model]()
+
+      modelActor ! ModelCommand.GetModel(probe.ref)
+
+      val receivedModel = probe.receiveMessage()
+      receivedModel shouldBe model1
+    }
+
+    "update the model when TrainingCompleted is received" in {
       val model1 = createModel(1L)
       val model2 = createModel(999L)
-
-      val modelActor = spawn(ModelActor(model1))
-      val probe = createTestProbe[Double]()
-      val testPoint = Point2D(0.5, 0.5)
-
-      modelActor ! ModelCommand.GetPrediction(testPoint, probe.ref)
-      val prediction1 = probe.receiveMessage()
+      val modelActor = spawn(ModelActor(model1, testOptimizer))
+      val probe = createTestProbe[Model]()
 
       modelActor ! ModelCommand.TrainingCompleted(model2)
-      modelActor ! ModelCommand.GetPrediction(testPoint, probe.ref)
-      val prediction2 = probe.receiveMessage()
+      modelActor ! ModelCommand.GetModel(probe.ref)
 
-      prediction1 should not be prediction2
+      probe.receiveMessage() shouldBe model2
     }
 
-    "return same predictions with same model" in {
+    "correctly merge models when SyncModel is received" in {
+      val localModel = createModel(1L)
+      val remoteModel = createModel(2L)
+      val modelActor = spawn(ModelActor(localModel, testOptimizer))
+      val probe = createTestProbe[Model]()
+
+      modelActor ! ModelCommand.SyncModel(remoteModel)
+      modelActor ! ModelCommand.GetModel(probe.ref)
+
+      val mergedModel = probe.receiveMessage()
+      mergedModel.network shouldNot be(localModel.network)
+      mergedModel.network shouldNot be(remoteModel.network)
+
+      val expectedNetwork = localModel.network averageWith remoteModel.network
+      mergedModel.network shouldBe expectedNetwork
+    }
+
+    /*
+    "provide metrics for the monitor" in {
       val model1 = createModel(1L)
-      val model2 = createModel(1L)
+      val modelActor = spawn(ModelActor(model1, testOptimizer))
+      val probe = createTestProbe[actors.monitor.MonitorProtocol.MonitorCommand.ViewUpdateResponse]()
 
-      val modelActor = spawn(ModelActor(model1))
-      val probe = createTestProbe[Double]()
-      val testPoint = Point2D(0.5, 0.5)
+      modelActor ! ModelCommand.GetMetrics(probe.ref)
 
-      modelActor ! ModelCommand.GetPrediction(testPoint, probe.ref)
-      val prediction1 = probe.receiveMessage()
-
-      modelActor ! ModelCommand.TrainingCompleted(model2)
-      modelActor ! ModelCommand.GetPrediction(testPoint, probe.ref)
-      val prediction2 = probe.receiveMessage()
-
-      prediction1 shouldBe prediction2
-    }
+      val response = probe.receiveMessage()
+      response.model shouldBe model1
+    }*/
   }
 }
