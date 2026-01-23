be02020bcdff7e54e9f02ac74ae14e3c271179a3
Domenico Francesco Giacobbi <domenico.giacobbi@studio.unibo.it>
2026-01-23 15:35:35 +0100

    ref: division into GossipActor, GossipProtocol, and GossipBehavior

--- src/main/scala/actors/gossip/GossipActor.scala
+++ src/main/scala/actors/gossip/GossipActor.scala
@@ -1,25 +1,16 @@
 package actors.gossip
 
-import actors.gossip.GossipProtocol.*
-
-import actors.ModelActor
-import actors.ModelActor.ModelCommand
-import actors.cluster.*
-import actors.monitor.MonitorProtocol.MonitorCommand
-import actors.trainer.TrainerActor.TrainerCommand
-
 import akka.actor.typed.{ActorRef, Behavior}
 import akka.actor.typed.scaladsl.Behaviors
-
-import akka.actor.typed.scaladsl.TimerScheduler
-
 import config.AppConfig
-import domain.network.Model
-
-import scala.util.Random
-import scala.concurrent.duration.*
+import actors.ModelActor.ModelCommand
+import actors.monitor.MonitorProtocol.MonitorCommand
+import actors.trainer.TrainerActor.TrainerCommand
+import actors.cluster.ClusterCommand
 
 object GossipActor:
+
+  export GossipProtocol.*
 
   def apply(
              modelActor: ActorRef[ModelCommand],
@@ -29,55 +20,16 @@ object GossipActor:
            )(using config: AppConfig): Behavior[GossipCommand] =
     Behaviors.setup: context =>
       Behaviors.withTimers: timers =>
-        timers.startTimerWithFixedDelay(TickGossip,
-                                        TickGossip,
-                                        config.gossipInterval)
-        context.log.info("GossipActor started: Peer-to-peer synchronization active.")
-        active(modelActor, monitorActor, trainerActor, clusterManager)
+        timers.startTimerWithFixedDelay(
+          GossipCommand.TickGossip,
+          GossipCommand.TickGossip,
+          config.gossipInterval
+        )
 
-  private def active(
-                      modelActor: ActorRef[ModelCommand],
-                      monitorActor: ActorRef[MonitorCommand],
-                      trainerActor: ActorRef[TrainerCommand],
-                      clusterManager: ActorRef[ClusterCommand]
-                    ): Behavior[GossipCommand] =
-    Behaviors.receive: (context, message) =>
-      message match
-        case TickGossip =>
-          clusterManager ! NodesRefRequest(replyTo = )
-          Behaviors.same
-        case SendModelToPeer(model, target) =>
-          target ! HandleRemoteModel(model)
-          Behaviors.same
-        case HandleRemoteModel(remoteModel) =>
-          context.log.info("Gossip: Received remote model. Triggering synchronization.")
-          modelActor ! ModelActor.ModelCommand.SyncModel(remoteModel)
-          Behaviors.same
-
-        //New node trigger Inizialize
-        //GossipCommand.Inizialize
-        //peer UP
-        case SpreadCommand(cmd) =>
-          context.log.info(s"Gossip: Broadcasting control signal $cmd to all known peers.")
-          //peers.foreach(_ ! GossipCommand.HandleControlCommand(cmd))
-          Behaviors.same
-
-        case HandleControlCommand(cmd) =>
-          context.log.info(s"Gossip: Executing remote control command: $cmd")
-          cmd match
-            case GlobalStart =>
-              clusterManager ! StartSimulation
-            case GlobalPause  =>
-              monitorActor ! MonitorCommand.InternalPause
-              trainerActor ! TrainerCommand.Pause
-            case GlobalResume =>
-              monitorActor ! MonitorCommand.InternalResume
-              trainerActor ! TrainerCommand.Resume
-            case GlobalStop   =>
-              monitorActor ! MonitorCommand.InternalStop
-              trainerActor ! TrainerCommand.Stop
-          Behaviors.same
-
-        case _ => Behaviors.unhandled
-        
-        //metrics consensus
+        GossipBehavior(
+          context,
+          modelActor,
+          monitorActor,
+          trainerActor,
+          clusterManager
+        ).active()
---
+++ src/main/scala/actors/gossip/GossipBehavior.scala
@@ -0,0 +1,54 @@
+package actors.gossip
+
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import actors.gossip.GossipProtocol.*
+import actors.ModelActor
+import actors.ModelActor.ModelCommand.Initialize
+import actors.cluster.{ClusterCommand, NodesRefRequest, StartSimulation}
+import actors.trainer.TrainerActor.TrainerCommand
+import actors.monitor.MonitorProtocol.MonitorCommand
+import domain.network.Model
+
+private[gossip] class GossipBehavior(
+                                      context: ActorContext[GossipCommand],
+                                      modelActor: ActorRef[ModelActor.ModelCommand],
+                                      monitorActor: ActorRef[MonitorCommand],
+                                      trainerActor: ActorRef[TrainerCommand],
+                                      clusterManager: ActorRef[ClusterCommand]
+                                    ):
+
+  def active(): Behavior[GossipCommand] =
+    Behaviors.receive: (context, message) =>
+      message match
+        case GossipCommand.TickGossip =>
+          Behaviors.same
+        case GossipCommand.SendModelToPeer(model, target) =>
+          context.log.info(s"Gossip: Sending local model weights to peer")
+          target ! GossipCommand.HandleRemoteModel(model)
+          Behaviors.same
+        case GossipCommand.HandleRemoteModel(remoteModel) =>
+          context.log.info("Remote model received: initiating local synchronization.")
+          modelActor ! ModelActor.ModelCommand.SyncModel(remoteModel)
+          Behaviors.same
+        case GossipCommand.HandleControlCommand(cmd) =>
+          context.log.info(s"Executing remote control command: $cmd")
+          cmd match
+            case ControlCommand.GlobalStart =>
+              clusterManager ! StartSimulation
+              monitorActor ! MonitorCommand.StartSimulation
+              modelActor ! Initialize()
+            case ControlCommand.GlobalPause =>
+              monitorActor ! MonitorCommand.InternalPause
+              trainerActor ! TrainerCommand.Pause
+            case ControlCommand.GlobalResume =>
+              monitorActor ! MonitorCommand.InternalResume
+              trainerActor ! TrainerCommand.Resume
+            case ControlCommand.GlobalStop =>
+              monitorActor ! MonitorCommand.InternalStop
+              trainerActor ! TrainerCommand.Stop
+          Behaviors.same
+
+        case _ =>
+          context.log.warn("Received unhandled gossip message.")
+          Behaviors.unhandled
