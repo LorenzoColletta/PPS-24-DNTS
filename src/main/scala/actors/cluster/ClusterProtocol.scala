a4f54faa8b15f43f94d3826beb5144581dd6e2ab
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-02-04 16:29:09 +0100

    refactor: cluster's commands name is more akka styling coherent

--- src/main/scala/actors/cluster/ClusterProtocol.scala
+++ src/main/scala/actors/cluster/ClusterProtocol.scala
@@ -5,7 +5,7 @@ import akka.cluster.ClusterEvent.{MemberEvent, ReachabilityEvent}
 /**
  * Defines the internal protocol of messages handled by cluster behaviors.
  */
-sealed trait ClusterCommand
+sealed trait ClusterProtocol
 
 /**
  * Wraps an Akka Cluster [[MemberEvent]] for internal handling.
@@ -13,7 +13,7 @@ sealed trait ClusterCommand
  * @param e
  * The original cluster member event (e.g., [[akka.cluster.ClusterEvent.MemberUp]], [[akka.cluster.ClusterEvent.MemberRemoved]])
  */
-final case class MemberCommand(e: MemberEvent) extends ClusterCommand
+final case class MemberProtocol(e: MemberEvent) extends ClusterProtocol
 
 /**
  * Wraps an Akka Cluster [[ReachabilityEvent]] for internal handling.
@@ -21,14 +21,24 @@ final case class MemberCommand(e: MemberEvent) extends ClusterCommand
  * @param e
  * The original cluster reachability event (e.g., [[akka.cluster.ClusterEvent.ReachableMember]], [[akka.cluster.ClusterEvent.UnreachableMember]])
  */
-final case class ReachabilityCommand(e: ReachabilityEvent) extends ClusterCommand
+final case class ReachabilityProtocol(e: ReachabilityEvent) extends ClusterProtocol
 
 /**
  * Signals that the cluster simulation should start.
  */
-case object StartSimulation extends ClusterCommand
+case object StartSimulation extends ClusterProtocol
 
 /**
  * Signals that the cluster simulation should stop.
  */
-case object StopSimulation extends ClusterCommand
+case object StopSimulation extends ClusterProtocol
+
+/**
+ * Signals the failure against joining the cluster  
+ */
+case object JoinTimeout extends ClusterProtocol
+
+/**
+ * Signals that the seed could be unreachable
+ */
+case object SeedUnreachable extends ClusterProtocol
