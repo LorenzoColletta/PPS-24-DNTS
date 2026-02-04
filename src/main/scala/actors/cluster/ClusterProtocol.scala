e5bd43fb0d3a605e4e5b8835dc23694848c0d935
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-02-04 16:33:27 +0100

    fix: better distinguishes between cluster event-related commands and application-related commands

--- src/main/scala/actors/cluster/ClusterProtocol.scala
+++ src/main/scala/actors/cluster/ClusterProtocol.scala
@@ -1,44 +1,78 @@
 package actors.cluster
 
-import akka.cluster.ClusterEvent.{MemberEvent, ReachabilityEvent}
+import akka.actor.Address
+import akka.cluster.ClusterEvent.{MemberRemoved, MemberUp, ReachableMember, UnreachableMember}
 
 /**
- * Defines the internal protocol of messages handled by cluster behaviors.
+ * Defines the internal protocol of messages handled by cluster behavior.
  */
-sealed trait ClusterProtocol
+object ClusterProtocol:
 
-/**
- * Wraps an Akka Cluster [[MemberEvent]] for internal handling.
- *
- * @param e
- * The original cluster member event (e.g., [[akka.cluster.ClusterEvent.MemberUp]], [[akka.cluster.ClusterEvent.MemberRemoved]])
- */
-final case class MemberProtocol(e: MemberEvent) extends ClusterProtocol
+  sealed trait ClusterMemberCommand
 
-/**
- * Wraps an Akka Cluster [[ReachabilityEvent]] for internal handling.
- *
- * @param e
- * The original cluster reachability event (e.g., [[akka.cluster.ClusterEvent.ReachableMember]], [[akka.cluster.ClusterEvent.UnreachableMember]])
- */
-final case class ReachabilityProtocol(e: ReachabilityEvent) extends ClusterProtocol
+  sealed trait NodeEvent extends ClusterMemberCommand
 
-/**
- * Signals that the cluster simulation should start.
- */
-case object StartSimulation extends ClusterProtocol
+  /**
+   * Wraps an Akka Cluster [[MemberUp]] for internal handling.
+   *
+   * @param node
+   * The original cluster member event.
+   */
+  final case class NodeUp(node: MemberUp) extends NodeEvent
 
-/**
- * Signals that the cluster simulation should stop.
- */
-case object StopSimulation extends ClusterProtocol
+  /**
+   * Wraps an Akka Cluster [[MemberRemoved]] for internal handling.
+   *
+   * @param node
+   * The original cluster member event.
+   */
+  final case class NodeRemoved(node: MemberRemoved) extends NodeEvent
 
-/**
- * Signals the failure against joining the cluster  
- */
-case object JoinTimeout extends ClusterProtocol
+  /**
+   * Wraps an Akka Cluster [[ReachableMember]] for internal handling.
+   *
+   * @param node
+   * The original cluster member event.
+   */
+  final case class NodeReachable(node: ReachableMember) extends NodeEvent
 
-/**
- * Signals that the seed could be unreachable
- */
-case object SeedUnreachable extends ClusterProtocol
+  /**
+   * Wraps an Akka Cluster [[UnreachableMember]] for internal handling.
+   *
+   * @param node
+   * The original cluster member event.
+   */
+  final case class NodeUnreachable(node: UnreachableMember) extends NodeEvent
+
+
+  sealed trait AppClusterCommand extends ClusterMemberCommand
+
+  /**
+   * Signals that the cluster simulation should start.
+   */
+  case object StartSimulation extends AppClusterCommand
+
+  /**
+   * Signals that the cluster simulation should stop.
+   */
+  case object StopSimulation extends AppClusterCommand
+
+
+  sealed trait InternalEvent extends ClusterMemberCommand
+
+  /**
+   * Signals the end of the time within which to successfully join the cluster.
+   */
+  case object JoinTimeout extends InternalEvent
+
+  /**
+   * Signals the end time from which a node is considered no longer recoverable.
+   *
+   * @param node the node no longer recoverable.
+   */
+  final case class UnreachableTimeout(node: Address) extends InternalEvent
+
+  /**
+   * Signals the end time from which the Seed is considered no longer recoverable.
+   */
+  case object SeedUnreachableTimeout extends InternalEvent
