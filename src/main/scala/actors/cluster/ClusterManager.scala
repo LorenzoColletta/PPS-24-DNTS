583e34b807a97416e3fe640cd1a21c3564aaf51a
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-02-05 12:18:37 +0100

    fix: implements FSM through phase + policies rather than multiple behaviors

--- src/main/scala/actors/cluster/ClusterManager.scala
+++ src/main/scala/actors/cluster/ClusterManager.scala
@@ -1,41 +1,119 @@
 package actors.cluster
 
+import actors.cluster.ClusterProtocol.*
+import actors.cluster.effect.*
+import actors.cluster.timer.*
+import actors.cluster.*
+import actors.monitor.MonitorProtocol.MonitorCommand
+import actors.discovery.GossipPeerDiscovery.DiscoveryCommand
+import actors.monitor.MonitorProtocol.MonitorCommand.PeerCountChanged
+import actors.root.RootProtocol.{NodeRole, RootCommand}
 import akka.actor.typed.*
 import akka.actor.typed.scaladsl.*
 import akka.cluster.ClusterEvent.{MemberEvent, ReachabilityEvent}
-import actors.monitor.MonitorProtocol.MonitorCommand
-import actors.GossipActor.GossipCommand
-import akka.cluster.typed.{Cluster, Subscribe}
+import akka.cluster.ClusterEvent
+import akka.cluster.typed.{Cluster, Down, Leave, Subscribe}
 
 /**
- * Entry point actor for listening to Akka Cluster events.
+ * Actor responsible to the management of the cluster.
  *
- * @param monitor
- * Actor reference to the monitoring actor that tracks peer counts.
- * @param gossip
- * Actor reference to the gossip actor that propagates membership changes to peers.
- * @return
- * A [[Behavior]] that manages cluster subscriptions and delegates events to
- * [[ClusterBehaviors.connecting]].
+ * Depending on the current phase it manages:
+ *  - the connection of a new node
+ *  - the loss of connection with a known node
  */
 object ClusterManager:
 
+  /**
+   * It creates the ClusterManager behavior.
+   *
+   * Initializes:
+   *  - cluster event subscriptions
+   *  - bootstrap timer
+   *
+   * @param initialState        the initial state
+   * @param timersDuration      timers configuration
+   * @param monitorActor        the monitor actor
+   * @param receptionistManager the receptionist manager actor
+   * @param rootActor           the root actor
+   * @return the actor's behavior
+   */
   def apply(
-    monitor: ActorRef[MonitorCommand],
-    gossip: ActorRef[GossipCommand]
-  ): Behavior[ClusterCommand] =
+    initialState: ClusterState,
+    timersDuration: ClusterTimers,
+    monitorActor: ActorRef[MonitorCommand],
+    receptionistManager: ActorRef[DiscoveryCommand],
+    rootActor: ActorRef[RootCommand]
+  ): Behavior[ClusterMemberCommand] =
     Behaviors.setup { context =>
 
-      val memberEventAdapter: ActorRef[MemberEvent] = context.messageAdapter(MemberCommand.apply)
-      Cluster(context.system).subscriptions ! Subscribe(memberEventAdapter, classOf[MemberEvent])
+      val cluster = Cluster(context.system)
 
-      val reachabilityEventAdapter: ActorRef[ReachabilityEvent] = context.messageAdapter(ReachabilityCommand.apply)
-      Cluster(context.system).subscriptions ! Subscribe(reachabilityEventAdapter, classOf[ReachabilityEvent])
+      val adapterMemberUp: ActorRef[ClusterEvent.MemberUp] = context.messageAdapter(NodeUp.apply)
+      val adapterMemberRemoved: ActorRef[ClusterEvent.MemberRemoved] = context.messageAdapter(NodeRemoved.apply)
+      val adapterMemberReachable: ActorRef[ClusterEvent.ReachableMember] = context.messageAdapter(NodeReachable .apply)
+      val adapterMemberUnreachable: ActorRef[ClusterEvent.UnreachableMember] = context.messageAdapter(NodeUnreachable.apply)
 
-      ClusterBehaviors.connecting(
-        context,
-        ClusterMembership.empty,
-        monitor,
-        gossip
-      )
+      cluster.subscriptions ! Subscribe(adapterMemberUp, classOf[ClusterEvent.MemberUp])
+      cluster.subscriptions ! Subscribe(adapterMemberRemoved, classOf[ClusterEvent.MemberRemoved])
+      cluster.subscriptions ! Subscribe(adapterMemberReachable, classOf[ClusterEvent.ReachableMember]      )
+      cluster.subscriptions ! Subscribe(adapterMemberUnreachable, classOf[ClusterEvent.UnreachableMember])
+
+      Behaviors.withTimers { timers =>
+        timers.startSingleTimer(BootstrapTimerId, JoinTimeout, timersDuration.bootstrapCheck)
+        runningBehavior(context, timers, initialState, timersDuration, monitorActor, receptionistManager, rootActor)
+      }
     }
+
+  private def runningBehavior(
+    context: ActorContext[ClusterMemberCommand],
+    timers: TimerScheduler[ClusterMemberCommand],
+    state: ClusterState,
+    timersDuration: ClusterTimers,
+    monitorActor: ActorRef[MonitorCommand],
+    receptionistManager: ActorRef[DiscoveryCommand],
+    rootActor: ActorRef[RootCommand]
+  ): Behavior[ClusterMemberCommand] =
+    Behaviors.receiveMessage { msg =>
+
+      val (stateAfterHandle, effects) = handle(state, msg)
+
+      val newState = effects.foldLeft(stateAfterHandle) {
+        case (state, ChangePhase(to)) =>
+          state.copy(phase = to)
+
+        case (state, RemoveNodeFromMembership(nodeId)) =>
+          state.copy(view = state.view.removeNode(nodeId))
+
+        case (state, _) => state
+      }
+
+      effects.collect { case e: Action => e }
+        .foreach(effect => ClusterEffects(state, context, timers, effect, timersDuration, monitorActor,
+          receptionistManager, rootActor))
+
+      runningBehavior(context, timers, newState, timersDuration, monitorActor, receptionistManager, rootActor)
+    }
+
+  private def handle(
+    state: ClusterState,
+    msg: ClusterMemberCommand
+  ): (ClusterState, List[Effect]) = {
+
+    val newView =
+      msg match
+        case e: NodeEvent =>
+          membership.MembershipPolicy.update(state.view, e)
+        case _ => state.view
+
+    val newState = state.copy(view = newView)
+
+    val policy = state.phase match
+      case Bootstrap => BootstrapPolicy
+      case Joining   => JoiningPolicy
+      case Running   => RunningPolicy
+
+
+    val effects = policy.decide(newState, msg)
+
+    (newState, effects)
+  }
