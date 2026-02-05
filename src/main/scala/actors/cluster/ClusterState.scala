219392a68441d27ece0618f0986204c038875d02
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-02-05 12:12:00 +0100

    feat: defines the cluster's state

---
+++ src/main/scala/actors/cluster/ClusterState.scala
@@ -0,0 +1,22 @@
+package actors.cluster
+
+import actors.cluster.membership.ClusterMembership
+import actors.root.RootProtocol.NodeRole
+
+sealed trait Phase
+case object Bootstrap extends Phase
+case object Joining extends Phase
+case object Running extends Phase
+
+/**
+ * Cluster state from the current node perspective.
+ * @param phase application phase
+ * @param role role of current node
+ * @param view current node's view on cluster membership
+ */
+final case class ClusterState(
+  phase: Phase,
+  role: NodeRole,
+  view: ClusterMembership
+)
+
