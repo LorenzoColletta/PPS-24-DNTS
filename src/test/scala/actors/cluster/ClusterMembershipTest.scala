fe3ad321b1b0763d4a19127e68264ba260ca7fdc
LorenzoColletta <lorenzo.colletta@studio.unibo.it>
2026-02-11 16:03:07 +0100

    feat: tests the ClusterManager

--- src/test/scala/actors/cluster/ClusterMembershipTest.scala
+++ src/test/scala/actors/cluster/ClusterMembershipTest.scala
@@ -1,120 +1,110 @@
 package actors.cluster
 
+import actors.cluster.membership.ClusterMembership
 import akka.actor.Address
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
-class ClusterMembershipTest extends AnyFunSuite with Matchers {
+class ClusterMembershipTest extends AnyFunSuite with Matchers:
 
   private def address(id: Int): Address =
     Address("akka", "ClusterSystem", s"host$id", 2550 + id)
 
-  test("empty membership has no peers") {
+  test("empty membership has no nodes"):
     val m = ClusterMembership.empty
 
-    m.peers shouldBe empty
-    m.unreachable shouldBe empty
+    m.nodesUp shouldBe empty
+    m.nodesUnreachable shouldBe empty
     m.total shouldBe 0
     m.available shouldBe 0
-  }
 
-  test("addPeer adds a peer to the membership") {
+  test("addNode adds a peer to the membership"):
     val a = address(1)
 
-    val m = ClusterMembership.empty.addPeer(a)
+    val m = ClusterMembership.empty.addNode(a)
 
-    m.peers shouldBe Set(a)
-    m.unreachable shouldBe empty
+    m.nodesUp shouldBe Set(a)
+    m.nodesUnreachable shouldBe empty
     m.total shouldBe 1
     m.available shouldBe 1
-  }
 
-  test("addPeer is idempotent") {
+  test("addNode is idempotent"):
     val a = address(1)
 
-    val m1 = ClusterMembership.empty.addPeer(a)
-    val m2 = m1.addPeer(a)
+    val m1 = ClusterMembership.empty.addNode(a)
+    val m2 = m1.addNode(a)
 
     m2 shouldBe m1
-  }
 
-  test("removePeer removes a peer from peers and unreachable") {
+  test("removeNode removes a peer from nodes and unreachable"):
     val a = address(1)
 
     val m = ClusterMembership.empty
-      .addPeer(a)
+      .addNode(a)
       .markUnreachable(a)
-      .removePeer(a)
+      .removeNode(a)
 
-    m.peers shouldBe empty
-    m.unreachable shouldBe empty
+    m.nodesUp shouldBe empty
+    m.nodesUnreachable shouldBe empty
     m.total shouldBe 0
-  }
 
-  test("markUnreachable marks a peer as unreachable") {
+  test("markUnreachable marks a peer as unreachable"):
     val a = address(1)
 
     val m = ClusterMembership.empty
-      .addPeer(a)
+      .addNode(a)
       .markUnreachable(a)
 
-    m.unreachable shouldBe Set(a)
+    m.nodesUnreachable shouldBe Set(a)
     m.available shouldBe 0
-  }
 
-  test("markUnreachable is idempotent") {
+  test("markUnreachable is idempotent"):
     val a = address(1)
 
     val m1 = ClusterMembership.empty
-      .addPeer(a)
+      .addNode(a)
       .markUnreachable(a)
 
     val m2 = m1.markUnreachable(a)
 
     m2 shouldBe m1
-  }
 
-  test("markReachable removes a peer from unreachable") {
+  test("markReachable removes a peer from unreachable"):
     val a = address(1)
 
     val m = ClusterMembership.empty
-      .addPeer(a)
+      .addNode(a)
       .markUnreachable(a)
       .markReachable(a)
 
-    m.unreachable shouldBe empty
+    m.nodesUnreachable shouldBe empty
     m.available shouldBe 1
-  }
 
-  test("markReachable is idempotent") {
+  test("markReachable is idempotent"):
     val a = address(1)
 
-    val m1 = ClusterMembership.empty.addPeer(a)
+    val m1 = ClusterMembership.empty.addNode(a)
     val m2 = m1.markReachable(a)
 
     m2 shouldBe m1
-  }
 
-  test("unreachable peers are always a subset of peers") {
+  test("unreachable nodes are always a subset of nodes"):
     val a = address(1)
 
     val m = ClusterMembership.empty
-      .addPeer(a)
+      .addNode(a)
       .markUnreachable(a)
-      .removePeer(a)
+      .removeNode(a)
 
-    m.unreachable shouldBe empty
-  }
+    m.nodesUnreachable shouldBe empty
 
-  test("available peers never exceed total peers") {
+  test("available nodes never exceed total nodes"):
     val a = address(1)
     val b = address(2)
 
     val m = ClusterMembership.empty
-      .addPeer(a)
-      .addPeer(b)
+      .addNode(a)
+      .addNode(b)
       .markUnreachable(a)
 
     m.available should be <= m.total
-  }
-}
